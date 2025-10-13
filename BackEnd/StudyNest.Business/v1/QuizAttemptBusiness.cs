5209320f92d7ba513cd05fbfd79f71944acb8b19
Vu Nguyen <z1venttt@gmail.com>
2025-10-13 19:24:11 +0700

    Mark The QuizAttempt Update For Better Reliability

--- BackEnd/StudyNest.Business/v1/QuizAttemptBusiness.cs
+++ BackEnd/StudyNest.Business/v1/QuizAttemptBusiness.cs
@@ -134,6 +134,8 @@ namespace StudyNest.Business.v1
                         int totalQuestions = parsedQuestions.Count;
                         int correctAnswers = existingAttempt.QuizAttemptAnswers.Where(x => x.IsCorrect).Count();
                         existingAttempt.Score = (int)Math.Round((double)(correctAnswers * 100) / totalQuestions);
+                        // Mark the attempt as completed
+                        _dbContext.QuizAttempts.Update(existingAttempt);
                         // Update the attempt as completed
                         if (await _dbContext.SaveChangesAsync() > 0)
                         {
